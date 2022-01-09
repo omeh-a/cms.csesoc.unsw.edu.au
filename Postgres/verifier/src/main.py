75acf42661d3e6c08be93eb886a298c77535e28d
Hanyuan Li <hanyuan.work@gmail.com>
2022-01-09 14:12:47 +1100

    feat(verifier): add some command-line features

--- Postgres/verifier/src/main.py
+++ Postgres/verifier/src/main.py
@@ -2,12 +2,14 @@ import psycopg
 import sys
 import yaml
 
+# TODO: use Docker instead
 USERNAME = "postgres"
 PASSWORD = "test"
 DB = "cms_testing_db"
 HOST = "localhost"
 PORT = 1234
 
+# Fetch all the columns (name and data type) of a given table.
 def get_columns(cursor, table):
     cursor.execute(f"""
         SELECT column_name, udt_name
@@ -22,6 +24,15 @@ def get_columns(cursor, table):
 
     return records
 
+# Given a column specified in a YAML file, check if it matches with our
+# existing records.
+def check_column(column, records):
+    name, data_type = column["name"], column["type"]
+
+    if records[name] != data_type:
+        raise TypeError(f"Expected type {data_type} for table {name}, got {records[name]}")
+
+# Try connecting to the database
 try:
     conn = psycopg.connect(f"""
     dbname={DB}
@@ -33,21 +44,33 @@ try:
 
     cursor = conn.cursor()
 except (Exception, psycopg.Error) as e:
-    print(e)
+    raise e
 
 filename = sys.argv[1]
 
 with open(filename) as FILE:
     contents = yaml.load(FILE, Loader=yaml.Loader)
+    passed = 0
+    total = 0
 
     for table in contents.keys():
+        print(f"Scanning through table '{table}': ", end="")
+
         records = get_columns(cursor, table)
         existing = contents[table]
 
         for column in existing:
-            name, data_type = column["name"], column["type"]
+            try:
+                check_column(column, records)
+                passed += 1
+                
+                print(".", end="")
+            except TypeError as e:
+                print("")
+                print(e)
 
-            if records[name] != data_type:
-                raise TypeError(f"Expected type {data_type} for table {name}, got {records[name]}")
+            total += 1
 
-print("Success!")
+        print("")
+
+print(f"{passed}/{total} total test cases passed!")
