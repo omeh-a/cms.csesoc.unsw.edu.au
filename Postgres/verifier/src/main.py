2720d97cf0afa4e98753b4e490d126cb2f06cf37
Hanyuan Li <hanyuan.work@gmail.com>
2022-01-10 12:33:42 +1100

    feat(verifier): added more error checking

--- Postgres/verifier/src/main.py
+++ Postgres/verifier/src/main.py
@@ -21,16 +21,19 @@ def get_columns(cursor, table):
 
     for name, data_type in cursor:
         records[name] = data_type
-
+    
     return records
 
 # Given a column specified in a YAML file, check if it matches with our
 # existing records.
-def check_column(column, records):
+def check_column(column, records, table):
     name, data_type = column["name"], column["type"]
 
+    if name not in records.keys():
+        raise NameError(f"Column '{name}' does not exist in table '{table}'")
+
     if records[name] != data_type:
-        raise TypeError(f"Expected type {data_type} for table {name}, got {records[name]}")
+        raise TypeError(f"Expected type '{data_type}' for column {name}, got '{records[name]}'")
 
 # Try connecting to the database
 try:
@@ -46,31 +49,38 @@ try:
 except (Exception, psycopg.Error) as e:
     raise e
 
+# Main program
 filename = sys.argv[1]
 
 with open(filename) as FILE:
     contents = yaml.load(FILE, Loader=yaml.Loader)
+
     passed = 0
     total = 0
+    errors = []
 
     for table in contents.keys():
         print(f"Scanning through table '{table}': ", end="")
 
         records = get_columns(cursor, table)
-        existing = contents[table]
+        table_checks = contents[table]
+        total += len(table_checks)
 
-        for column in existing:
+        for column in table_checks:
             try:
-                check_column(column, records)
+                check_column(column, records, table)
                 passed += 1
-                
-                print(".", end="")
-            except TypeError as e:
-                print("")
-                print(e)
 
-            total += 1
+                print(".", end="")
+            except (TypeError, NameError) as e:
+                print("X")
+                errors.append(e)
 
         print("")
+
+    for error in errors:
+        print(error)
+
+    print("")
 
 print(f"{passed}/{total} total test cases passed!")
